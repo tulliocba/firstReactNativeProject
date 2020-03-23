29b0f11f47ad48dae15c8b0026c0424c38398beb
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-23 11:45:42 -0300

    Config eslint

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}> Welcome to React Native</Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
 });
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}> Welcome to React Native</Text>
+        </View>
+    );
+}
