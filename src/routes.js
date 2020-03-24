7e58cdeaca62ec0f6e367cc4a6c460fc628614a4
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-24 18:14:28 -0300

    Configurando StatusBar

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from './pages/Main';
@@ -8,26 +9,38 @@ const Stack = createStackNavigator();
 
 function Routes() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerTitleAlign: 'center',
-                    headerTintColor: '#FFF',
-                    headerBackTitleVisible: false,
-                    headerStyle: {
-                        backgroundColor: '#7159c1',
-                    },
-                }}
-            >
-                <Stack.Screen
-                    name="Main"
-                    component={Main}
-                    options={{
-                        title: 'Olá Mundo',
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Main"
+                    screenOptions={{
+                        headerTitleAlign: 'center',
+                        headerTintColor: '#FFF',
+                        headerBackTitleVisible: false,
+                        headerStyle: {
+                            backgroundColor: '#7159c1',
+                        },
                     }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+                >
+                    <Stack.Screen
+                        name="Main"
+                        component={Main}
+                        options={{
+                            title: 'Main',
+                        }}
+                    />
+
+                    <Stack.Screen
+                        name="User"
+                        component={User}
+                        options={{
+                            title: 'Usuários',
+                        }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
     );
 }
 
