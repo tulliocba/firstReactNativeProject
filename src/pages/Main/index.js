d38482c0832d5080b707d676df9b90da609ab91b
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-24 19:16:41 -0300

    Estilizando o formulãrio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-    return <Container />;
+    return (
+        <Container>
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar Usuário"
+                />
+                <SubmitButton>
+                    <Icon name="add" size={20} color="#FFF" />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
