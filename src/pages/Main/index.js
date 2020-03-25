31754bfcb51293adfc99d91e3e5e2d1a30c50316
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-25 11:20:22 -0300

    Salvando no storage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,18 +2,50 @@
 import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Keyboard } from 'react-native';
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../../services/api';
+
+import {
+    Container,
+    Form,
+    Input,
+    SubmitButton,
+    List,
+    User,
+    Avatar,
+    Name,
+    Bio,
+    ProfileButtom,
+    ProfileButtomText,
+} from './styles';
 
 export default class Main extends Component {
     state = {
         newUser: '',
         users: [],
+        loading: false,
     };
 
+    async componentDidMount() {
+        const users = await AsyncStorage.getItem('users');
+        if (users) {
+            this.setState({ users: JSON.parse(users) });
+        }
+    }
+
+    async componentDidUpdate(_, prevState) {
+        const { users } = this.state;
+
+        if (prevState.users !== users) {
+            AsyncStorage.setItem('users', JSON.stringify(users));
+        }
+    }
+
     handleAddUser = async () => {
+        this.setState({ loading: true });
+
         const { users, newUser } = this.state;
         const response = await api.get(`/users/${newUser}`);
 
@@ -27,13 +59,14 @@ export default class Main extends Component {
         this.setState({
             users: [...users, data],
             newUser: '',
+            loading: false,
         });
 
         Keyboard.dismiss();
     };
 
     render() {
-        const { newUser } = this.state;
+        const { users, newUser, loading } = this.state;
 
         return (
             <Container>
@@ -49,10 +82,33 @@ export default class Main extends Component {
                         returnKeyType="send"
                         onSubmitEditing={this.handleAddUser}
                     />
-                    <SubmitButton onPress={this.handleAddUser}>
-                        <Icon name="add" size={20} color="#FFF" />
+                    <SubmitButton
+                        loading={loading}
+                        onPress={this.handleAddUser}
+                    >
+                        {loading ? (
+                            <ActivityIndicator color="#FFF" />
+                        ) : (
+                            <Icon name="add" size={20} color="#FFF" />
+                        )}
                     </SubmitButton>
                 </Form>
+                <List
+                    data={users}
+                    keyExtractor={(user) => user.login}
+                    renderItem={({ item }) => (
+                        <User>
+                            <Avatar source={{ uri: item.avatar }} />
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+                            <ProfileButtom onPress={this.handleAddUser}>
+                                <ProfileButtomText>
+                                    Ver Perfil
+                                </ProfileButtomText>
+                            </ProfileButtom>
+                        </User>
+                    )}
+                />
             </Container>
         );
     }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -29,4 +29,51 @@ export const SubmitButton = styled(RectButton)`
     border-radius: 4px;
     margin-left: 10px;
     padding: 0 12px;
+    opacity: ${(props) => (props.loading ? 0.6 : 1)};
+`;
+
+export const List = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+})`
+    margin-top: 20px;
+`;
+
+export const User = styled.View`
+    align-items: center;
+    margin: 0 20px 30px;
+`;
+export const Avatar = styled.Image`
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    background: #eee;
+`;
+export const Name = styled.Text`
+    font-size: 14px;
+    color: #333;
+    font-weight: bold;
+    margin-top: 4px;
+    text-align: center;
+`;
+export const Bio = styled.Text.attrs({})`
+    font-size: 13px;
+    line-height: 18px;
+    color: #999;
+    margin-top: 5px;
+    text-align: center;
+`;
+export const ProfileButtom = styled(RectButton)`
+    margin-top: 10px;
+    align-self: stretch;
+    border-radius: 4px;
+    background: #7159c1;
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+`;
+export const ProfileButtomText = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
 `;
