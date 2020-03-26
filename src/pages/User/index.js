4bf7404822b8a3a556645f8e12d5f8fc4c829f2b
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-26 19:15:19 -0300

    Buscando repositíros favoritados

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,21 @@
 /* eslint-disable react/state-in-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-// import { Container } from './styles';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+} from './styles';
 
 export default class User extends Component {
     state = {
@@ -21,7 +32,35 @@ export default class User extends Component {
     }
 
     render() {
-        return <View />;
+        const { stars } = this.state;
+        const { route } = this.props;
+        const { user } = route.params;
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+
+                <Stars
+                    data={stars}
+                    keyExtractor={(star) => String(star.id)}
+                    renderItem={({ item }) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{ uri: item.owner.avatar_url }}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Starred>
+                    )}
+                />
+            </Container>
+        );
     }
 }
 
